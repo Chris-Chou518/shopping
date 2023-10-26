b3844ef3af15e6ec8fc34d59af11c7b1fc9becdb
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-26 13:24:55 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const routes = require('./routes')
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
+module.exports = app
