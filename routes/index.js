d1b33ee1b640bdb03a97114f1e144a9ea9e6db6f
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-26 18:53:46 +0800

    Feat: flash message & sign up verification

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const router = express.Router()
 const itemController = require('../controllers/item-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/items', itemController.getItems)
 router.use('/', (req, res) => res.redirect('/items'))
-
+router.use('/', generalErrorHandler)
 module.exports = router
