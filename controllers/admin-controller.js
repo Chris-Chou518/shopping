a0dc206845888014317e999bf63c5bf423e3351d
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-27 20:51:29 +0800

    Feat: create item

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -6,6 +6,23 @@ const adminController = {
     })
       .then(items => res.render('admin/items', { items }))
       .catch(err => next(err))
+  },
+  createItem: (req, res) => {
+    return res.render('admin/create-item')
+  },
+  postItem: (req, res, next) => {
+    const { name, description, price } = req.body
+    if (!name) throw new Error('Item name is required')
+    Item.create({
+      name,
+      description,
+      price
+    })
+      .then(() => {
+        req.flash('success_messages', 'Item was successfully created!')
+        res.redirect('/admin/items')
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
-router.get('/items', authenticatedAdmin, adminController.getItems)
+router.get('/items/create', adminController.createItem)
+router.post('/items', adminController.postItem)
+router.get('/items', adminController.getItems)
 router.use('/', (req, res) => res.redirect('/admin/items'))
 module.exports = router
