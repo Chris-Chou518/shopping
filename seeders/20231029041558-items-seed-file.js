4fe5aa352e027e6c93c27409dadfe0463248ac1e
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-29 13:50:13 +0800

    Feat: add seed data

---
+++ seeders/20231029041558-items-seed-file.js
@@ -0,0 +1,21 @@
+'use strict'
+const faker = require('faker')
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Items',
+      Array.from({ length: 50 }, () => ({
+        name: faker.name.findName(),
+        price: Math.floor(Math.random() * 1000),
+        description: faker.lorem.text(),
+        image: `https://loremflickr.com/320/240/cat/?random=${Math.random() * 100}`,
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Items', {})
+  }
+}
