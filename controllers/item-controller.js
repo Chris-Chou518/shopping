538648908118d65726ebace1213c7612b6dba9c8
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-31 10:04:00 +0800

    Feat: add item page

--- controllers/item-controller.js
+++ controllers/item-controller.js
@@ -13,6 +13,19 @@ const itemController = {
         }))
         return res.render('items', { items: data })
       })
+      .catch(err => next(err))
+  },
+  getItem: (req, res, next) => {
+    return Item.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(item => {
+        if (!item) throw new Error('Item does not exist!')
+        return res.render('item', { item })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = itemController
