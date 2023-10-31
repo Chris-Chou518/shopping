866cb63a08b52fd42ad7ca02c82ed1b0890f404a
Chris-Chou518 <ttlyes496@gmail.com>
2023-10-31 09:31:49 +0800

    Feat: add items index page

--- controllers/item-controller.js
+++ controllers/item-controller.js
@@ -1,6 +1,18 @@
+const { Item, Category } = require('../models')
 const itemController = {
   getItems: (req, res, next) => {
-    return res.render('items')
+    return Item.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(items => {
+        const data = items.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('items', { items: data })
+      })
   }
 }
 module.exports = itemController
