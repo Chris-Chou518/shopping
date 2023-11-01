ef9b33c52805d803a9c37fe7e7c4cbb06496e741
Chris-Chou518 <ttlyes496@gmail.com>
2023-11-01 11:51:10 +0800

    Feat: add post comment on item page

---
+++ controllers/comment-controller.js
@@ -0,0 +1,26 @@
+const { Comment, User, Item } = require('../models')
+const commentController = {
+  postComments: (req, res, next) => {
+    const { text, itemId } = req.body
+    const userId = req.user.id
+    if (!text) throw new Error('Comment is required!')
+    return Promise.all([
+      User.findByPk(userId),
+      Item.findByPk(itemId)
+    ])
+      .then(([user, item]) => {
+        if (!user) throw new Error('User does not exist!')
+        if (!item) throw new Error('Item does not exist!')
+        return Comment.create({
+          text,
+          userId,
+          itemId
+        })
+      })
+      .then(() => {
+        res.redirect(`/items/${itemId}`)
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = commentController
